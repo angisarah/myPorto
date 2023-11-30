1ed749e5a78f1ae7d80fab69368b1fc096e413f0
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-30 10:54:43 +0300

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -26,7 +26,7 @@ with st.container():
     # Horizontal layout for logo and navigation buttons
     st.markdown(
         f"""
-        <div style="position: fixed; width: 100%; background-color: white; z-index: 1000;">
+        <div style="position: fixed;right :32px width: 100%; background-color: white; z-index: 1000;">
             <div style="display: flex; align-items: center; justify-content: space-between;">
                 <div style = "position: relative; left: 0px;">
                     <!-- Logo Image -->
