c55e67345dc01f758357c9afdf79666c5061d8be
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-30 10:25:34 +0300

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -30,7 +30,7 @@ with st.container():
             <div style="display: flex; align-items: center; justify-content: space-between;">
                 <div>
                     <!-- Logo Image -->
-                    <img src="https://i.imgur.com/kjwJq12.png" alt="Logo" style="width: auto; height: 50px;">
+                    <img src="https://i.imgur.com/kjwJq12.png" alt="Logo" style="width: auto; height: 24px;">
                 </div>
                 <div style="display: flex; gap: 20px;">
                     <!-- Navigation Buttons -->
