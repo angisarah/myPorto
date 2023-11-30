6fdbd4f5917dcba9e3b9e1e2013c0d7733e2b75e
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-30 13:46:46 +0300

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -33,7 +33,7 @@ with st.container():
                     <!-- Logo Image -->
                     <img src="https://i.imgur.com/kjwJq12.png" alt="Logo" style="width: auto; height: 24px;">
                 </div>
-                <div style="margin-right: 120px;display: flex; gap: 20px;">
+                <div style="margin-right: 132px;display: flex; gap: 20px;">
                     <!-- Navigation Buttons -->
                     <button onclick="location.href='#'" style="background-color: transparent; padding: 10px 20px; border: none; cursor: pointer;">About</button>
                     <button onclick="location.href='#'" style="background-color: transparent; padding: 10px 20px; border: none; cursor: pointer;">Home</button>
